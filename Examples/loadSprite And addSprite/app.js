78904db3aeafb8b9aec6d8998141640cca8e5287
danmoop <danmoople@gmail.com>
2017-08-15 20:15:33 +0300

    Add files via upload

---
+++ Examples/loadSprite And addSprite/app.js
@@ -0,0 +1,10 @@
+var game = new MoopleGame(900, 600, "canv", {update: update});
+
+firstScene = new Scene();
+firstScene.show();
+cat = firstScene.addSprite('Cat.png', 0,0, 100, 100);
+
+function update()
+{
+	
+}
